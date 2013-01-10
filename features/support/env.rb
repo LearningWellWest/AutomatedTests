0da60a47b2ab1d7f202ea26b3b2ce2ffbe954281
Anton Danielsson <anton.danielsson@gmail.com>
2013-01-10 11:51:26 +0100

    Added a config-file for storing the page URL at one place. Also added an example on how a test can look like

---
+++ features/support/env.rb
@@ -0,0 +1,21 @@
+require 'selenium-webdriver'
+require 'capybara/cucumber'
+require 'rspec/expectations'
+require 'rails'
+Capybara.default_driver = :selenium
+APP_CONFIG = YAML.load_file("#{Rails.root}config.yml")[Rails.env]
+
+
+
+Before do
+    @driver = Selenium::WebDriver.for :firefox
+    @base_url = APP_CONFIG['standard_uri']
+    @accept_next_alert = true
+    @driver.manage.timeouts.implicit_wait = 30
+    @verification_errors = []
+  end
+
+After do
+    @driver.quit
+    @verification_errors.should == []
+end
---
+++ features/youtube_search.feature
@@ -0,0 +1,19 @@
+# language: sv
+
+Egenskap: YouTube har en sökfunktion.
+
+Bakgrund:
+  Givet att jag är på YouTube
+
+Scenariomall: Sök efter en term
+  När jag skriver i fältet "masthead-search-term" texten <sträng>
+  Och trycker på "search-btn"
+  Så ska jag se <resultat>
+  
+Exempel:
+	| sträng	| resultat 						|
+	| "banan"		| "Sean Banan - Gott nytt jul" 	|
+	| "gurka"		| "ddddddddddddddjshuhugyiygu" 	|
+
+
+
