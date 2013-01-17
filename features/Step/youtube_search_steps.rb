c73468934a0bc3ee38a80b05feda748855b8b8a0
antda <anton.danielsson@gmail.com>
2013-01-17 12:10:44 +0100

    Added feature for youtube-search

---
+++ features/Step/youtube_search_steps.rb
@@ -0,0 +1,21 @@
+require 'cucumber/formatter/unicode'
+require 'rbconfig'
+require "selenium-webdriver"
+require "rspec"
+include RSpec::Expectations
+
+
+Given /^att jag skriver i faltet "(.*?)" texten "(.*?)"$/ do |field, value|
+	@driver.get('http://www.youtube.com' + "/")
+	@driver.find_element(:id, field).clear
+    @driver.find_element(:id, field).send_keys value
+end
+
+When /^trycker pa "(.*?)"$/ do |button|
+  @driver.find_element(:id, button).click
+end
+
+Then /^ska jag se "(.*?)"$/ do |textString|
+  @driver.find_element(:css, "BODY").text.should =~ /^[\s\S]*#{textString}[\s\S]*$/
+end
+
