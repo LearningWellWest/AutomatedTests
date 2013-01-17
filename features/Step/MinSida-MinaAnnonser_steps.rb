a83cbee009958e9b0215f19942ccc3c68fdd6864
patrikjohansson87 <patrikjohansson87@hotmail.com>
2013-01-17 14:35:05 +0100

    ändringar

--- features/Step/MinSida-MinaAnnonser_steps.rb
+++ features/Step/MinSida-MinaAnnonser_steps.rb
@@ -18,10 +18,10 @@ När /^jag klickar på "min sida"$/ do
 	@driver.find_element(:css, "BODY").text.should =~ /^[\s\S]*Min sida[\s\S]*$/
 end
 
-När /^jag sedan klickar på "(.*?)"$/ do |arg1|
+När /^jag sedan klickar på Mina annonser$/ do
 	@driver.find_element(:link, "Mina annonser").click
 end
 
-Så /^ska jag komma till "(.*?)"$/ do |arg1|
+Så /^ska jag komma till Mina annonser$/ do
     @driver.find_element(:css, "BODY").text.should =~ /^[\s\S]*Publicerad[\s\S]*$/
 end
--- features/Step/MinSida-MinaAvtal.rb
+++ features/Step/MinSida-MinaAvtal.rb
@@ -18,10 +18,10 @@ När /^jag klickar på "min sida"$/ do
 	@driver.find_element(:css, "BODY").text.should =~ /^[\s\S]*Min sida[\s\S]*$/
 end
 
-När /^jag sedan klickar på "(.*?)"$/ do |arg1|
+När /^jag sedan klickar på Avtal$/ do
 	@driver.find_element(:link, "Avtal").click
 end
 
-Så /^ska jag komma till "(.*?)"$/ do |arg1|
+Så /^ska jag komma till Avtal$/ do
     @driver.find_element(:css, "BODY").text.should =~ /^[\s\S]*Avtal[\s\S]*$/
 end
