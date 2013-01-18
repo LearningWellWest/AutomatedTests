128af6ea0046cf9f03cf7e691a3cd7536fe3d99e
PatrikE <patrik_helltorp@hotmail.com>
2013-01-18 14:01:35 +0100

    Snart kommer mer stepfiler

---
+++ features/Laggatillannons.rb
@@ -0,0 +1,15 @@
+Givet /^är på Skapa annons sidan$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+När /^jag fyller i Annonsuppgifter$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+När /^klickar på Spara annons$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Så /^ska min annons vara sparad$/ do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/SkapaAnnons-Laggatillannons.feature
@@ -0,0 +1,12 @@
+# language: sv
+
+Egenskap: Skapa annons
+
+Bakgrund: En kund vill sälja en sak
+
+Scenario: Inloggad Skapa annons
+	Givet att jag är inloggad
+	Och är på Skapa annons sidan
+	När jag fyller i Annonsuppgifter
+	Och klickar på Spara annons
+	Så ska min annons vara sparad
