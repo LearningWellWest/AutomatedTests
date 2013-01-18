20dbc4dd4c0d6ebb548c973f847c5fb1f7acae7b
patrikjohansson87 <patrikjohansson87@hotmail.com>
2013-01-18 14:07:44 +0100

    Ändringar i steps

---
+++ features/MinSida-Link.feature
@@ -0,0 +1,8 @@
+# language: sv
+
+Egenskap: Min sida länk
+
+Scenario: Inloggad min sida länk
+	Givet att jag är inloggad
+	När jag klickar på Min sida
+	Så ska jag komma till Min sida
--- features/Step/klick-MinSida.rb
+++ features/Step/klick-MinSida.rb
@@ -1,5 +1,10 @@
 # encoding: utf-8
+
 När /^jag klickar på Min sida$/ do
 	@driver.find_element(:link, "Min sida").click
+	@driver.find_element(:css, "BODY").text.should =~ /^[\s\S]*Min sida[\s\S]*$/
+end
+
+Så /^ska jag komma till Min sida$/ do
 	@driver.find_element(:css, "BODY").text.should =~ /^[\s\S]*Min sida[\s\S]*$/
 end
