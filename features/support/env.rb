a82f804e085d732f85d305084042faeeacff9254
antda <anton.danielsson@gmail.com>
2013-01-23 14:31:51 +0100

    Tests now run locally again. Gotta save our precious minutes

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,15 +8,15 @@ APP_CONFIG = YAML.load_file("#{Rails.root}config.yml")[Rails.env]
 
 
 Before do
-    #@driver = Selenium::WebDriver.for :firefox
+    @driver = Selenium::WebDriver.for :firefox
 
-	caps = Selenium::WebDriver::Remote::Capabilities.firefox
-	caps.platform = 'Linux'
-	caps[:name] = "Alla test"
-	@driver = Selenium::WebDriver.for(
-  	:remote,
-  	:url => "http://antda:7af6cc4d-5c19-4f09-a325-4c3d8221b80a@ondemand.saucelabs.com:80/wd/hub",
-  	:desired_capabilities => caps)
+	#caps = Selenium::WebDriver::Remote::Capabilities.firefox
+	#caps.platform = 'Linux'
+	#caps[:name] = "Alla test"
+	#@driver = Selenium::WebDriver.for(
+  	#:remote,
+  	#:url => "http://antda:7af6cc4d-5c19-4f09-a325-4c3d8221b80a@ondemand.saucelabs.com:80/wd/hub",
+  	#:desired_capabilities => caps)
 
 
 
