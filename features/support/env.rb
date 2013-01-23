71d6da71c3345daca0b0cc1358424b51750e1501
antda <anton.danielsson@gmail.com>
2013-01-23 14:21:29 +0100

    The tests now run on SauceLabs. We have 200minutes for free

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,7 +8,19 @@ APP_CONFIG = YAML.load_file("#{Rails.root}config.yml")[Rails.env]
 
 
 Before do
-    @driver = Selenium::WebDriver.for :firefox
+    #@driver = Selenium::WebDriver.for :firefox
+
+	caps = Selenium::WebDriver::Remote::Capabilities.firefox
+	caps.platform = 'Linux'
+	caps[:name] = "Alla test"
+	@driver = Selenium::WebDriver.for(
+  	:remote,
+  	:url => "http://antda:7af6cc4d-5c19-4f09-a325-4c3d8221b80a@ondemand.saucelabs.com:80/wd/hub",
+  	:desired_capabilities => caps)
+
+
+
+
     @base_url = APP_CONFIG['standard_uri']
     @accept_next_alert = true
     @driver.manage.timeouts.implicit_wait = 30
