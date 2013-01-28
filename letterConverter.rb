90718870b8ffc2e900da407d26a80c2009deed1e
antda <anton.danielsson@gmail.com>
2013-01-28 11:05:10 +0100

    Letter converter fix

--- letterConverter.rb
+++ letterConverter.rb
@@ -1,3 +1,4 @@
+puts "Running char-converter"
 ARGV.each do |file_name|
   text = File.read(file_name)
 
@@ -10,6 +11,11 @@ ARGV.each do |file_name|
 #replace ö and Ö
   text = text.gsub("ö", "&ouml;")
   text = text.gsub("Ö", "&Ouml;")
+
+# N&auml;r -> När
+text = text.gsub("N&auml;r", "När")
+# S&aring; -> Så
+text = text.gsub("S&aring;", "Så")
 
 File.open(file_name, "w") { |file| file.puts text }
 end
