db63f0164b996c2574798e73d5673cf699d3b76b
antda <anton.danielsson@gmail.com>
2013-01-29 15:59:49 +0100

    Now SauceLabs gets the feature name as session name. Added configurations for running tests on linux/firefox and winXP/chrome

---
+++ features/support/chromeWinXP-SauceLabs.rb
@@ -0,0 +1,15 @@
+Before do |scenario|
+
+	caps = Selenium::WebDriver::Remote::Capabilities.chrome
+	caps.platform = 'Windows 2003'
+	caps[:name] = scenario.feature.name
+	@driver = Selenium::WebDriver.for(
+  	:remote,
+  	:url => "http://antda:7af6cc4d-5c19-4f09-a325-4c3d8221b80a@ondemand.saucelabs.com:80/wd/hub",
+	:desired_capabilities => caps)
+
+    @base_url = APP_CONFIG['standard_uri']
+    @accept_next_alert = true
+    @driver.manage.timeouts.implicit_wait = 30
+    @verification_errors = []
+end
