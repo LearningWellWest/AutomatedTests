0dd303e60660aaf313450ebb4b0f2564661e5557
antda <anton.danielsson@gmail.com>
2013-03-07 14:35:38 +0100

    Made a feature to test picture-uploading.

--- features/step/annonsera.rb
+++ features/step/annonsera.rb
@@ -68,6 +68,24 @@ När /^jag fyller i Publiceringsstart med "(.*?)"$/ do |arg1|
   element_annonsera_postalcode.send_keys arg1
 end
 
+#Bild
+När /^jag lägger till bilden "(.*?)"$/ do |imgname|
+  annonsera_upload_picture(imgname)
+end
+
+Så /^ska bilduppladdningen lyckas$/ do
+  @driver.find_elements(:class,"alert-success").size.should >0
+end
+
+När /^jag laddar upp många bilder så att dem tillsammans överstiger (\d+)mb$/ do |arg1|
+  i =0
+  while i <6 do
+    annonsera_upload_picture("flower.jpg")
+    sleep 2
+    i=i+1
+  end
+sleep 10
+end
 
 När /^jag skapar en annons med rubriken "(.*?)" samt kategorin "(.*?)"$/ do |arg1, arg2|
   fill_CreateAd(arg1,"Har tröttnat på den",arg2,"10","Testavdelningen","Testförvaltningen","Storgatan 1","Södra rondellen 2","65538","Karlstad","Trappa 2 till höger","kokosboll.jpg")
